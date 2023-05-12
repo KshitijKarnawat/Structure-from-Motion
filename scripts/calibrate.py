318291ea8577875802340569571aa1915faccc8e
Kshitij Karnawat <kshitijkarnawat009@gmail.com>
2023-05-11 22:00:39 -0400

    Ported to Project 3 code

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -1,48 +1,62 @@
-import cv2
+#!/usr/bin/env python3
+
+"""
+ * @copyright Copyright (c) 2023
+ * @file zhang.py
+ * @author Kshitij Karnawat (kshitij@umd.edu)
+ * @brief Question 2 for Porject 3
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * 
+"""
+
 import numpy as np
-import os
+import cv2 as cv
 import glob
 
-world_coords= [(0,0,0),(22.5,0,0),(45,0,0),(67.5,0,0),(90,0,0),(112.5,0,0),(129,0,0),(150.5,0,0),(172,0,0),
-              (0,22.5,0),(22.5,22.5,0),(45,22.5,0),(67.5,22.5,0),(90,22.5,0),(112.5,22.5,0),(129,22.5,0),(150.5,22.5,0),(172,22.5,0),
-              (0,45,0),(22.5,45,0),(45,45,0),(67.5,45,0),(90,45,0),(112.5,45,0),(129,45,0),(150.5,45,0),(172,45,0),
-              (0,67.5,0),(22.5,67.5,0),(45,67.5,0),(67.5,67.5,0),(90,67.5,0),(112.5,67.5,0),(129,67.5,0),(150.5,67.5,0),(172,67.5,0),
-               (0,90,0),(22.5,90,0),(45,90,0),(67.5,90,0),(90,90,0),(112.5,90,0),(129,90,0),(150.5,90,0),(172,90,0),
-               (0,112.5,0),(22.5,112.5,0),(45,112.5,0),(67.5,112.5,0),(90,112.5,0),(112.5,112.5,0),(129,112.5,0),(150.5,112.5,0),(172,112.5,0)]
+ground_truth = [] 
+detected_points = [] 
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-path = r'../Data/Calibration images'
-world_coords = np.array(world_coords, dtype= np.float32)
-world_points = []
-img_points = []
-image_frames = glob.glob(os.path.join(path,'*.png'))
-for i in image_frames:
-    img = cv2.imread(i)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, corners = cv2.findChessboardCorners(gray_img, (9,6), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+points = []
+
+point = np.array([0,0,0],dtype=np.float32)
+for i in range(0,6):
+    point[1] = 0
+    for j in range(0,9):
+        points.append([point[0],point[1],point[2]])
+        point[1] += 21.5
+    point[0] += 21.5 
+
+points = np.array(points, dtype=np.float32)
+
+images = glob.glob('Images/*.jpg')
+
+for i in images:
+    img = cv.imread(i)
+    img = cv.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)), interpolation=cv.INTER_AREA)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    ret, corners = cv.findChessboardCorners(gray, (9,6), cv.CALIB_CB_ADAPTIVE_THRESH)
+
     if ret == True:
-        world_points.append(world_coords)
-        corners_mod = cv2.cornerSubPix(gray_img,corners, (11,11), (-1,-1), criteria)
-        img_points.append(corners_mod)
-        cv2.drawChessboardCorners(img, (9,6), corners_mod, ret)
-        resized_img= cv2.resize(img,(1920,1080),interpolation= cv2.INTER_AREA)
-        cv2.imshow('img', resized_img)
-        cv2.waitKey(500)
-cv2.destroyAllWindows()
+        ground_truth.append(points)
+        detected_points.append(corners)
+        cv.drawChessboardCorners(img, (9,6), corners, ret)
+        cv.imshow('img', img)
+        cv.waitKey()
 
-ret, K, dist, R_vec, T_vec = cv2.calibrateCamera(world_points, img_points, gray_img.shape[::-1], None, None)
-per_frame_rep_error = []
-mean_error = 0
-for i in range(len(world_points)):
-    pred_imgpoints, _ = cv2.projectPoints(world_points[i], R_vec[i], T_vec[i], K, dist)
-    error = cv2.norm(img_points[i], pred_imgpoints, cv2.NORM_L2)/len(pred_imgpoints)
-    per_frame_rep_error.append(error)
-    mean_error += error
-frame= 1
-for err in per_frame_rep_error:
-    print("Reprojection error for {} frame is: {}".format(frame,err))
-    frame+=1
-print("*********************************************")
-print( "Average reprojection error across all frames: {}".format(mean_error/len(world_points)))
-print("*********************************************")
-print("The intrinsic matrix (K) is: {}".format(K))
+cv.destroyAllWindows()
+
+ret, intrinsic_matrix, distortion, rotation, translation = cv.calibrateCamera(ground_truth, detected_points, gray.shape[::-1], None, None)
+
+reprojection_error = 0
+for i in range(len(ground_truth)):
+    imgpoints, _ = cv.projectPoints(ground_truth[i], rotation[i], translation[i], intrinsic_matrix, distortion)
+    error = cv.norm(detected_points[i], imgpoints, cv.NORM_L2)/len(imgpoints)
+    print("Reprojection Error of Image",i,"=",error,)
+    reprojection_error += error
+
+print("Mean Reprojection Error =",reprojection_error/13)
+
+print("Intrinsic Matrix:", intrinsic_matrix)
